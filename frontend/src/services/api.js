9016eccf191890c1fb66a090bab22f63f3f159c8
nithin05060423 <nithin05060423@users.noreply.github.com>
2026-07-13 16:49:28 +0530

    Fix CORS and API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://107.20.129.72:8000",
-    headers: {
-        "Content-Type": "application/json"
-    }
+  baseURL: "http://107.20.129.72:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
