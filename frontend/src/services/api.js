a1a4b4032677e3e5d063553a88398f987d755559
nithin05060423 <nithin05060423@users.noreply.github.com>
2026-07-13 16:24:12 +0530

    Fix API URL for EC2 deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "http://107.20.129.72:8000",
     headers: {
         "Content-Type": "application/json"
     }
